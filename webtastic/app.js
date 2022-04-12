417275f136e5b534dcaaf23ad5634808d6162770
Tristan Miller <7457323+tristancurry@users.noreply.github.com>
2022-04-12 23:02:57 +0930

    Update app.js
    Numerator/denominator logic working fine now

--- webtastic/app.js
+++ webtastic/app.js
@@ -1,9 +1,30 @@
-let MAX_SHAPES = 2;
-
+const SHAPES_MAX = 4;
 const numerator = document.getElementById('numerator');
 const denominator = document.getElementById('denominator');
+const numeratorSelector = document.getElementById('numerator-selector');
+const denominatorSelector = document.getElementById('denominator-selector');
 
-let setSliderMaximum = (slider, max) => {
+//Initial values
+const NUMERATOR_INITIAL = 3;
+const DENOMINATOR_INITIAL = 7;
+const SHAPES_INITIAL = 2;
+
+let shapes_current = SHAPES_INITIAL;
+
+updateSliderValue(denominatorSelector, DENOMINATOR_INITIAL);
+updateSliderValue(numeratorSelector, NUMERATOR_INITIAL);
+
+
+
+function updateSliderValue (slider, value) {
+  slider.value = `${value}`;
+  slider.setAttribute('value', `${value}`);
+  let sliderInput = new Event('input', {bubbles: true, cancelable: true});
+  slider.dispatchEvent(sliderInput);
+}
+
+
+function setSliderMaximum (slider, max) {
   let newMax = Math.round(max);
   if (newMax < 1) {newMax = 1;}
 
@@ -14,34 +35,42 @@ let setSliderMaximum = (slider, max) => {
   if (newMax != currentMax) {
     //1: set the new maximum value for the slider (if needed or valid!)
     slider.setAttribute('max', `${newMax}`);
-    console.log('changing the max');
 
     //2: update the current value of the slider as necessary
     //3: trigger any necessary events to make this manifest in other places.
-    if (currentValue > MAX_SHAPES*newMax) {
-      console.log('should be changin');
-      slider.value = `${MAX_SHAPES*newMax}`;
-      slider.setAttribute('value', `${MAX_SHAPES*newMax}`);
+    if (currentValue > newMax) {
+      slider.value = `${newMax}`;
+      slider.setAttribute('value', `${newMax}`);
       let sliderInput = new Event('input', {bubbles: true, cancelable: true});
       slider.dispatchEvent(sliderInput);
     }
   }
 }
 
-let setBigNumber = (position, value) => {
+function setBigNumber (position, value) {
   position.getElementsByTagName('span')[0].innerText = value;
 }
 
-let numeratorSelector = document.getElementById('numerator-selector');
-let denominatorSelector = document.getElementById('denominator-selector');
+function changeMaxShapes (value) {
+  //update shapes_current
+  shapes_current = Math.round(value);
+  if(shapes_current < 1) {shapes_current = 1;}
+
+  //update maximum value of numerator
+  let denominator_max = parseInt(denominatorSelector.value);
+  setSliderMaximum(numeratorSelector, shapes_current*denominator_max);
+
+}
 
 denominatorSelector.addEventListener('input', function (event) {
   let value = parseInt(event.target.value);
-  setSliderMaximum(numeratorSelector, value);
+  setSliderMaximum(numeratorSelector, shapes_current*value);
   setBigNumber(denominator, value);
+  //also update the visuals!
 });
 
 numeratorSelector.addEventListener('input', function (event) {
   let value = parseInt(event.target.value);
   setBigNumber(numerator, value);
+  //also update the visuals!
 });
